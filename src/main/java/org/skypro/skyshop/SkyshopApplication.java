07e059c6905f64515d8762c813303d6756a09c50
zor40 <zor40@mail.ru>
2026-03-01 13:48:10 +0500

    commit

---
+++ src/main/java/org/skypro/skyshop/SkyshopApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.skyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/skyshop/SkyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.skyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
